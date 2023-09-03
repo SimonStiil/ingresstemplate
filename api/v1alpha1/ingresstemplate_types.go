919fabfe824db9c1c4b22259e1441912ecc5fd1f
simon <130985998+SimonStiil@users.noreply.github.com>
2023-09-03 15:40:00 +0200

    first public draft of controller in kubebuilder 3.11.1

---
+++ api/v1alpha1/ingresstemplate_types.go
@@ -0,0 +1,140 @@
+/*
+Copyright 2023.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	networkingv1 "k8s.io/api/networking/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// IngressTemplateSpec defines the desired state of IngressTemplate
+type IngressTemplateSpec struct {
+	// +listType=atomic
+	// +optional
+	SecretReplacements []SecretReplacement `json:"secertReplacement,omitempty" protobuf:"bytes,1,opt,name=secertReplacement"`
+
+	// copied from networking.v1.types.go "type IngressSpec struct"
+
+	// IngressClassName is the name of the IngressClass cluster resource. The
+	// associated IngressClass defines which controller will implement the
+	// resource. This replaces the deprecated `kubernetes.io/ingress.class`
+	// annotation. For backwards compatibility, when that annotation is set, it
+	// must be given precedence over this field. The controller may emit a
+	// warning if the field and annotation have different values.
+	// Implementations of this API should ignore Ingresses without a class
+	// specified. An IngressClass resource may be marked as default, which can
+	// be used to set a default value for this field. For more information,
+	// refer to the IngressClass documentation.
+	// +optional
+	IngressClassName *string `json:"ingressClassName,omitempty" protobuf:"bytes,4,opt,name=ingressClassName"`
+
+	// DefaultBackend is the backend that should handle requests that don't
+	// match any rule. If Rules are not specified, DefaultBackend must be specified.
+	// If DefaultBackend is not set, the handling of requests that do not match any
+	// of the rules will be up to the Ingress controller.
+	// +optional
+	DefaultBackend *networkingv1.IngressBackend `json:"defaultBackend,omitempty" protobuf:"bytes,1,opt,name=defaultBackend"`
+
+	// TLS configuration. Currently the Ingress only supports a single TLS
+	// port, 443. If multiple members of this list specify different hosts, they
+	// will be multiplexed on the same port according to the hostname specified
+	// through the SNI TLS extension, if the ingress controller fulfilling the
+	// ingress supports SNI.
+	// +listType=atomic
+	// +optional
+	TLS []networkingv1.IngressTLS `json:"tls,omitempty" protobuf:"bytes,2,rep,name=tls"`
+
+	// A list of host rules used to configure the Ingress. If unspecified, or
+	// no rule matches, all traffic is sent to the default backend.
+	// +listType=atomic
+	// +optional
+	Rules []networkingv1.IngressRule `json:"rules,omitempty" protobuf:"bytes,3,rep,name=rules"`
+}
+
+type SecretReplacement struct {
+
+	// The name of the secret in the pod's namespace to select from.
+	SecretName string `json:"secretName,omitempty" protobuf:"bytes,1,opt,name=secretName"`
+
+	// What should be replaced in path or host.
+	// +optional
+	Replacement string `json:"replacement" protobuf:"bytes,2,opt,name=replacement"`
+	// The key of the secret to select from.  Must be a valid secret key.
+	// +optional
+	Key string `json:"key" protobuf:"bytes,3,opt,name=key"`
+}
+
+// IngressTemplateStatus defines the observed state of IngressTemplate
+type IngressTemplateStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+	Condition string `json:"condition,omitempty" protobuf:"bytes,1,rep,name=conditions"`
+
+	Secrets []SecretStatus `json:"secrets,omitempty" protobuf:"bytes,2,rep,name=secrets"`
+}
+
+type SecretStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+	Secret string `json:"secret,omitempty" protobuf:"bytes,1,key,name=secret"`
+	Status string `json:"status,omitempty" protobuf:"bytes,2,key,name=status"`
+	Sha1   string `json:"sha,omitempty" protobuf:"bytes,3,key,name=Sha"`
+}
+
+const (
+	// Conditions
+	AwaitingSecret string = "AwaitingSecret"
+	Created        string = "Created"
+	Failed         string = "Failed"
+	New            string = "New"
+
+	// SecretStatus
+	NotFound string = "NotFound"
+	Found    string = "Found"
+	Changed  string = "Changed"
+)
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// IngressTemplate is the Schema for the ingresstemplates API
+type IngressTemplate struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   IngressTemplateSpec   `json:"spec,omitempty"`
+	Status IngressTemplateStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// IngressTemplateList contains a list of IngressTemplate
+type IngressTemplateList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []IngressTemplate `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&IngressTemplate{}, &IngressTemplateList{})
+}
---
+++ internal/controller/ingresstemplate_controller.go
@@ -0,0 +1,338 @@
+/*
+Copyright 2023.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controller
+
+import (
+	"context"
+	"crypto/sha1"
+	"encoding/hex"
+	"fmt"
+	"strings"
+
+	"github.com/go-logr/logr"
+	"k8s.io/apimachinery/pkg/fields" // Required for Watching
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types" // Required for Watching
+
+	"k8s.io/client-go/tools/record"
+
+	corev1 "k8s.io/api/core/v1"
+	networkingv1 "k8s.io/api/networking/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/builder" // Required for Watching
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/handler" // Required for Watching
+	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/predicate" // Required for Watching
+	"sigs.k8s.io/controller-runtime/pkg/reconcile" // Required for Watching
+
+	// Required for Watching
+	networkingv1alpha1 "stiil.dk/ingresstemplate/api/v1alpha1"
+)
+
+// IngressTemplateReconciler reconciles a IngressTemplate object
+type IngressTemplateReconciler struct {
+	client.Client
+	Scheme   *runtime.Scheme
+	Recorder record.EventRecorder
+}
+
+type SecretIngressReplacement struct {
+	Secrets     map[string]string
+	Replacement string
+	Key         string
+	Sha1        string
+	Status      networkingv1alpha1.SecretStatus
+}
+
+//+kubebuilder:rbac:groups=networking.stiil.dk,resources=ingresstemplates,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=networking.stiil.dk,resources=ingresstemplates/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=networking.stiil.dk,resources=ingresstemplates/finalizers,verbs=update
+//+kubebuilder:rbac:groups="",resources=secrets,verbs=get;list;watch
+//+kubebuilder:rbac:groups="",resources=events,verbs=create
+//+kubebuilder:rbac:groups=networking.k8s.io,resources=ingresses,verbs=get;list;watch;create;update;patch;delete
+
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state.
+// TODO(user): Modify the Reconcile function to compare the state specified by
+// the IngressTemplate object against the actual cluster state, and then
+// perform operations to make the cluster state reflect the state specified by
+// the user.
+//
+// For more details, check Reconcile and its Result here:
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.15.0/pkg/reconcile
+func (r *IngressTemplateReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	log := log.FromContext(ctx)
+	log.WithValues("namespace", req.NamespacedName)
+	log.Info("fetching IngressTemplate resource in ")
+	var ingressTemplate networkingv1alpha1.IngressTemplate
+	if err := r.Get(ctx, req.NamespacedName, &ingressTemplate); err != nil {
+		log.Error(err, "Unable to fetch IngressTemplate")
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+	buildStatus(&ingressTemplate)
+
+	log.WithValues("secret_ingress_name", ingressTemplate.Name)
+	// TODO(user): your logic here
+	if err := r.cleanupOwnedResources(ctx, &ingressTemplate); err != nil {
+		log.Error(err, "failed to clean up old Owned resources for this IngressTemplate")
+		return ctrl.Result{}, err
+	}
+	sirs := make([]SecretIngressReplacement, len(ingressTemplate.Spec.SecretReplacements))
+	missingSecret := false
+	for index, secretReplacement := range ingressTemplate.Spec.SecretReplacements {
+		secret := corev1.Secret{}
+		err := r.Get(ctx, client.ObjectKey{Namespace: ingressTemplate.Namespace, Name: secretReplacement.SecretName}, &secret)
+		var currentSecretStatus networkingv1alpha1.SecretStatus
+		for _, secret := range ingressTemplate.Status.Secrets {
+			if secretReplacement.SecretName == secret.Secret {
+				currentSecretStatus = secret
+				break
+			}
+		}
+		if apierrors.IsNotFound(err) {
+			currentSecretStatus.Status = networkingv1alpha1.NotFound
+			log.Error(err, fmt.Sprintf("Failed to find Secret %q", secretReplacement.SecretName))
+			missingSecret = true
+			continue
+		}
+		if err != nil {
+			log.Error(err, "Unable to fetch Secret")
+			return ctrl.Result{}, client.IgnoreNotFound(err)
+		}
+		currentSecretStatus.Status = networkingv1alpha1.Found
+		hash := sha1.New()
+		Secrets := make(map[string]string, len(secret.Data))
+		for k, bv := range secret.Data {
+			Secrets[k] = string(bv)
+			hash.Write([]byte(k))
+			hash.Write(bv)
+		}
+		result := hash.Sum(nil)
+		sha1 := hex.EncodeToString(result[:7])
+		sir := SecretIngressReplacement{Secrets: Secrets, Replacement: secretReplacement.Replacement,
+			Key: secretReplacement.Key, Sha1: sha1, Status: currentSecretStatus}
+		sirs[index] = sir
+	}
+	ingress := networkingv1.Ingress{}
+	err := r.Client.Get(ctx, client.ObjectKey{Namespace: ingressTemplate.Namespace, Name: ingressTemplate.Name}, &ingress)
+	if apierrors.IsNotFound(err) {
+		if missingSecret {
+			log.Error(err, "failed to create Ingress resource due to missing secret")
+			ingressTemplate.Status.Condition = networkingv1alpha1.AwaitingSecret
+			r.Status().Update(ctx, &ingressTemplate)
+			return ctrl.Result{}, err
+		}
+		log.Info("could not find existing Ingress for IngressTemplate, creating one...")
+		ingress = *buildIngress(&log, &ingressTemplate, &sirs)
+		if err := r.Client.Create(ctx, &ingress); err != nil {
+			log.Error(err, "failed to create Ingress resource")
+			ingressTemplate.Status.Condition = networkingv1alpha1.Failed
+			r.Status().Update(ctx, &ingressTemplate)
+			return ctrl.Result{}, err
+		}
+		r.Recorder.Eventf(&ingressTemplate, corev1.EventTypeNormal, "Created", "Created Ingress %q", ingress.Name)
+		log.Info("created Ingress resource for IngressTemplate")
+		ingressTemplate.Status.Condition = networkingv1alpha1.Created
+		r.Status().Update(ctx, &ingressTemplate)
+		return ctrl.Result{}, nil
+	}
+	updateRules(&log, &ingress, &ingressTemplate, &sirs)
+	if err := r.Client.Update(ctx, &ingress); err != nil {
+		log.Error(err, "failed to update Ingress resource")
+		ingressTemplate.Status.Condition = networkingv1alpha1.Failed
+		r.Status().Update(ctx, &ingressTemplate)
+		return ctrl.Result{}, err
+	}
+	log.Info("updated Ingress resource for IngressTemplate")
+	r.Status().Update(ctx, &ingressTemplate)
+	return ctrl.Result{}, nil
+}
+
+func buildStatus(ingressTemplate *networkingv1alpha1.IngressTemplate) {
+	for _, replacement := range ingressTemplate.Spec.SecretReplacements {
+		found := false
+		for _, secret := range ingressTemplate.Status.Secrets {
+			if replacement.SecretName == secret.Secret {
+				found = true
+				break
+			}
+		}
+		if !found {
+			status := networkingv1alpha1.SecretStatus{Secret: replacement.SecretName, Status: networkingv1alpha1.New}
+			ingressTemplate.Status.Secrets = append(ingressTemplate.Status.Secrets, status)
+		}
+	}
+}
+
+const (
+	secretField = ".spec.secretReplacement.secretName"
+)
+
+func (r *IngressTemplateReconciler) findObjectForSecret(ctx context.Context, secret client.Object) []reconcile.Request {
+	attachedIngressTemplates := &networkingv1alpha1.IngressTemplateList{}
+	listOps := &client.ListOptions{
+		FieldSelector: fields.OneTermEqualSelector(secretField, secret.GetName()),
+		Namespace:     secret.GetNamespace(),
+	}
+	err := r.List(ctx, attachedIngressTemplates, listOps)
+	if err != nil {
+		return []reconcile.Request{}
+	}
+
+	requests := make([]reconcile.Request, len(attachedIngressTemplates.Items))
+	for i, item := range attachedIngressTemplates.Items {
+		requests[i] = reconcile.Request{
+			NamespacedName: types.NamespacedName{
+				Name:      item.GetName(),
+				Namespace: item.GetNamespace(),
+			},
+		}
+	}
+	return requests
+}
+
+var (
+	deploymentOwnerKey = ".metadata.controller"
+)
+
+func (r *IngressTemplateReconciler) cleanupOwnedResources(ctx context.Context, ingressTemplate *networkingv1alpha1.IngressTemplate) error {
+	log := log.FromContext(ctx)
+	log.Info("finding existing Owned Resources for SecretIngress resource")
+
+	// List all deployment resources owned by this MyKind
+	var ingressList networkingv1.IngressList
+	//fields := make(map[string]string)
+	//fields := map[string]string{deploymentOwnerKey: secretIngress.Name}
+	//fields[deploymentOwnerKey] = secretIngress.Name
+	if err := r.List(ctx, &ingressList, client.InNamespace(ingressTemplate.Namespace)); err != nil {
+		return err
+	}
+
+	deleted := 0
+	for _, ingress := range ingressList.Items {
+		mine := false
+		if len(ingress.OwnerReferences) > 0 {
+			for _, reference := range ingress.OwnerReferences {
+				mine = reference.Name == ingressTemplate.Namespace &&
+					reference.Kind == ingressTemplate.Kind &&
+					reference.APIVersion == ingressTemplate.APIVersion &&
+					reference.UID == ingressTemplate.UID
+			}
+
+		}
+		if !mine {
+			continue
+		}
+
+		if err := r.Client.Delete(ctx, &ingress); err != nil {
+			log.Error(err, "failed to delete Ingress resource")
+			return err
+		}
+		r.Recorder.Eventf(ingressTemplate, corev1.EventTypeNormal, "Deleted", "Deleted deployment %q", ingress.Name)
+		deleted++
+	}
+
+	log.Info("finished cleaning up old Deployment resources", "number_deleted", deleted)
+
+	return nil
+}
+
+func buildIngress(log *logr.Logger, ingressTemplate *networkingv1alpha1.IngressTemplate, sirs *[]SecretIngressReplacement) *networkingv1.Ingress {
+	ingress := networkingv1.Ingress{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:            ingressTemplate.Name,
+			Namespace:       ingressTemplate.Namespace,
+			OwnerReferences: []metav1.OwnerReference{*metav1.NewControllerRef(ingressTemplate, networkingv1alpha1.GroupVersion.WithKind("SecretIngress"))},
+		},
+		Spec: networkingv1.IngressSpec{
+			IngressClassName: ingressTemplate.Spec.IngressClassName,
+			DefaultBackend:   ingressTemplate.Spec.DefaultBackend,
+			TLS:              ingressTemplate.Spec.TLS,
+		},
+	}
+	updateRules(log, &ingress, ingressTemplate, sirs)
+	return &ingress
+}
+
+func updateRules(log *logr.Logger, ingress *networkingv1.Ingress, ingressTemplate *networkingv1alpha1.IngressTemplate, sirs *[]SecretIngressReplacement) {
+	ingress.Spec.Rules = nil
+	for _, rule := range ingressTemplate.Spec.Rules {
+		fixedRule := networkingv1.IngressRule{
+			IngressRuleValue: networkingv1.IngressRuleValue{
+				HTTP: &networkingv1.HTTPIngressRuleValue{Paths: []networkingv1.HTTPIngressPath{}}}}
+		//fixedRule := *rule.DeepCopy()
+		for _, secretReplacement := range *sirs {
+			hostToFix := rule.Host
+			for k, v := range secretReplacement.Secrets {
+				if strings.Contains(rule.Host, k) {
+					if log != nil {
+						log.Info(fmt.Sprintf("updateRule(Host): %q fixed with %q > %q ", rule.Host, k, v))
+					}
+					hostToFix = strings.Replace(hostToFix, k, v, -1)
+				}
+			}
+			fixedRule.Host = hostToFix
+			for _, path := range rule.HTTP.Paths {
+				pathToFix := path.Path
+				for k, v := range secretReplacement.Secrets {
+					if strings.Contains(pathToFix, k) {
+						oldPath := pathToFix
+						pathToFix = strings.Replace(pathToFix, k, v, -1)
+						if log != nil {
+							log.Info(fmt.Sprintf("updateRule(Path): %q fixed with %q > %q to %q", oldPath, k, v, pathToFix))
+						}
+					}
+				}
+				fixedPath := networkingv1.HTTPIngressPath{PathType: path.PathType, Backend: path.Backend, Path: pathToFix}
+				fixedRule.HTTP.Paths = append(fixedRule.HTTP.Paths, fixedPath)
+			}
+		}
+		ingress.Spec.Rules = append(ingress.Spec.Rules, fixedRule)
+	}
+	for _, secretReplacement := range *sirs {
+		if secretReplacement.Status.Sha1 != "" && secretReplacement.Status.Sha1 != secretReplacement.Sha1 {
+			secretReplacement.Status.Status = networkingv1alpha1.Changed
+		}
+		secretReplacement.Status.Sha1 = secretReplacement.Sha1
+	}
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *IngressTemplateReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	if err := mgr.GetFieldIndexer().IndexField(context.Background(), &networkingv1alpha1.IngressTemplate{}, secretField, func(rawObj client.Object) []string {
+		// Extract the ConfigMap name from the ConfigDeployment Spec, if one is provided
+		ingressTemplate := rawObj.(*networkingv1alpha1.IngressTemplate)
+		if ingressTemplate.Name == "" {
+			return nil
+		}
+		return []string{ingressTemplate.Name}
+	}); err != nil {
+		return err
+	}
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&networkingv1alpha1.IngressTemplate{}).
+		Owns(&networkingv1.Ingress{}).
+		Watches(
+			&corev1.Secret{},
+			handler.EnqueueRequestsFromMapFunc(r.findObjectForSecret),
+			builder.WithPredicates(predicate.ResourceVersionChangedPredicate{}),
+		).
+		Complete(r)
+}
